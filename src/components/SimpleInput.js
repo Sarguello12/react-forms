2c36b227ca38aa7640c4e1a2acb7b52b6337c399
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-08-29 12:17:53 -0500

    feat: added blur form functionality

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,16 +1,15 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState("");
-	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
 	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-	useEffect(() => {
-		if (enteredNameIsValid) {
-			console.log("Name input is valid!");
-		}
-	}, [enteredNameIsValid]);
+	const enteredNameIsValid = enteredName.trim() !== "";
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+	const nameInputBlurHandler = (event) => {
+		setEnteredNameTouched(true);
+	};
 
 	const nameInputChangeHandler = (event) => {
 		setEnteredName(event.target.value);
@@ -20,21 +19,15 @@ const SimpleInput = (props) => {
 		event.preventDefault();
 		setEnteredNameTouched(true);
 
-		if (enteredName.trim() === "") {
-			setEnteredNameIsValid(false);
+		if (!enteredNameIsValid) {
 			return;
 		}
 
-		setEnteredNameIsValid(true);
 		console.log(enteredName);
 
-		const enteredValue = nameInputRef.current.value;
-		console.log(enteredValue);
-
 		setEnteredName("");
+		setEnteredNameTouched(false);
 	};
-
-	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
 	const nameInputClasses = nameInputIsInvalid
 		? "form-control invalid"
@@ -45,10 +38,10 @@ const SimpleInput = (props) => {
 			<div className={nameInputClasses}>
 				<label htmlFor="name">Your Name</label>
 				<input
-					ref={nameInputRef}
 					type="text"
 					id="name"
 					onChange={nameInputChangeHandler}
+					onBlur={nameInputBlurHandler}
 				/>
 				{nameInputIsInvalid && (
 					<p className="error-text">Name must not be empty.</p>
